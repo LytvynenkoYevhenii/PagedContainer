d4aee2cf7c2a5d6977b4829515dfd07074adacee
Yevhenii Lytvynenko <litvinenko.evg@gmail.com>
2024-03-30 17:02:32 +0200

    Added Sample app

--- Package/Package.swift
+++ Package/Package.swift
@@ -16,9 +16,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PackageContainer"),
+            name: "PackageContainer",
+            path: "Sources"),
         .testTarget(
             name: "PackageContainerTests",
-            dependencies: ["PackageContainer"]),
+            dependencies: ["PackageContainer"],
+            path: "Tests"),
     ]
 )
---
+++ Sample/PackageContainerSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PackageContainerSample
+//
+//  Created by Yevhenii Lytvynenko on 30.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
